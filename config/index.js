b9be3c1247c49651d7442acf859861a927fc13bb
Ishola Ayomide <101745279+Ay4codes@users.noreply.github.com>
2023-09-13 17:18:27 +0100

    Updated Docs

--- config/index.js
+++ config/index.js
@@ -6,14 +6,6 @@ const config = {
     domain: {
         URL: 'https://hng-task-two-09r7.onrender.com'
     },
-    auth: {
-        jwt: {
-            JWT_SECRET: process.env.JWT_SECRET || "jwt-little-secret",
-            TOKEN_EXPIRES_IN: process.env.TOKEN_EXPIRES_IN ? ms(process.env.TOKEN_EXPIRES_IN) : ms("1h"),
-            REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN ? ms(process.env.REFRESH_TOKEN_EXPIRES_IN) : ms("30d"),
-        },
-        BCRYPT_SALT: Number(process.env.BCRYPT_SALT) || 10,
-    },
     secutiry: {
         apikey: {
             ACCESS_KEY: process.env.ACCESS_KEY
